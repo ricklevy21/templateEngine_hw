afcc7891a5c190c348320aa20c794637fa2d973e
Rick Levy <ricklevy21@gmail.com>
2020-06-05 11:15:34 -0600

    inquirer cl fully working. can add any and all employees to team

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//define dependecies
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -13,8 +14,7 @@ const render = require("./lib/htmlRenderer");
 const team = []
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+//functioin to initiate the process of building a team. starts with adding a manager
 const buildTeam = () => {
     inquirer.prompt([
         {
@@ -38,17 +38,21 @@ const buildTeam = () => {
             message: "Enter manager's office number:"
         }
     ]).then(answers => {
-        //console.log(answers)
+        //create a new manager object by using the Manager class constructor
         const manager = new Manager(answers.name, answers.id, answers.email, answers.office)
+        //add the manager to the team array
         team.push(manager)
-        console.log(team)
+        //console.log(team)
+        //call funtion to see if more people need to be added to the team
         addToTeam()
     })
+    //catch error
     .catch(err =>{
         console.log(err)
     })
 }
 
+//function to inquire if more people need to be added to the team
 const addToTeam = () => {
     inquirer.prompt([
         {
@@ -63,29 +67,82 @@ const addToTeam = () => {
         }
     ]).then(answers => {
         //console.log(answers)
+        //call the function that adds an intern if "intern" is selected
         if (answers.add === "Add an Intern"){
             addIntern()
+        } else if (answers.add === "Add and Engineer"){
+            addEngineer()
+        } else if (answers.add === "No, I am finished adding people to the team."){
+            render(team)
         }
     })
 }
 
+//function that allows user to add an intern to the team
 const addIntern = () => {
     inquirer.prompt([
         {
             type: "input",
-            name: "add",
-            message: "Would you like to add another person to your team?",
-            choices: [
-                "Add an Intern",
-                "Add and Engineer",
-                "No, I am finished adding people to the team."
-            ]
+            name: "name",
+            message: "Enter intern's name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter intern's ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter intern's email:"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Enter intern's school:"
         }
     ]).then(answers => {
-        //console.log(answers)
-        if (answers.add === "Add an Intern"){
-            addIntern()
+        //create a new intern object using the Intern class constuctor
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+        //add the intern to the team array
+        team.push(intern)
+        //console.log(team)
+        //call funtion to see if more people need to be added to the team
+        addToTeam()
+    })
+}
+
+//function that allows user to add an engineer to the team
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter engineer's name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter engineer's ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter engineer's email:"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter engineer's github username:"
         }
+    ]).then(answers => {
+        //create a new engineer object using the Intern class constuctor
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        //add the engineer to the team array
+        team.push(engineer)
+        //console.log(team)
+        //call funtion to see if more people need to be added to the team
+        addToTeam()
     })
 }
 
